dcfcc47d07a538389ce407fdb256f9f724377ec4
Mike John <123770954+itsmikejohn@users.noreply.github.com>
2023-09-03 14:49:36 +0800

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,3 +1,3 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient("https://nysbssfxeglmylhssnvn.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55c2Jzc2Z4ZWdsbXlsaHNzbnZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM3MjMzNzEsImV4cCI6MjAwOTI5OTM3MX0.fwbAAVNgLagOTrZYnjD0OMxIIf2AzZrgDDXanawSFAY");
+export const supabase = createClient("https://nysbssfxeglmylhssnvn.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55c2Jzc2Z4ZWdsbXlsaHNzbnZuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MzcyMzM3MSwiZXhwIjoyMDA5Mjk5MzcxfQ.j85bVoPWYcSI84hBNj9uoJpFAISZXdONQTwgou07A5g");
